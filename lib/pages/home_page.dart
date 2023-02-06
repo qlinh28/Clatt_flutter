88ce31c8dfe9c4e2ae4ff374014fcb8e93b0c3bc
linh <linhltq28@gmail.com>
2023-02-06 13:57:25 +0700

    add color

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
+        backgroundColor: Colors.deepPurple.shade300,
+        color: Colors.white,
+        activeColor: Colors.white,
+        tabBackgroundColor: Colors.deepPurple.shade400,
+        gap: 8,
+        padding: const EdgeInsets.all(16),
+        tabMargin: const EdgeInsetsDirectional.all(8),
         tabs: const [
           GButton(
             icon: Icons.home,
